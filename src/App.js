360e7bf1f21a1ae81a8b4e9ae082c9bf769035a5
azharhameed115 <35377348+azharhameed115@users.noreply.github.com>
2020-04-21 00:28:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { render } from "react-dom";
+
+const styles = {
+  fontsFamily: "sans-serif",
+  color: "Blue"
+};
+
+let id = 0;
+
+const Todo = props => (
+  <li>
+    <input
+      type="checkbox"
+      checked={props.todo.checked}
+      onChange={props.onToggle}
+    />
+    <button onClick={props.onDelete}>Delete</button>
+    <span>{props.todo.text}</span>
+  </li>
+);
+
+class CApp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    };
+  }
+
+  addTodo() {
+    const text = prompt("Todo Text plzz");
+    this.setState({
+      todos: [...this.state.todos, { id: id++, text: text, checked: false }]
+    });
+  }
+
+  removeTodo(id) {
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.id != id)
+    });
+  }
+
+  toggleTodo(id) {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id !== id) return todo;
+        return {
+          id: todo.id,
+          text: todo.text,
+          checked: !todo.checked
+        };
+      })
+    });
+  }
+
+  render() {
+    return (
+      <div style={styles}>
+        <div>Todo Count {this.state.todos.length}</div>
+        <div>
+          Completed {this.state.todos.filter(todo => todo.checked).length}
+        </div>
+        <button onClick={() => this.addTodo()}>Add Todo</button>
+        <ul>
+          {this.state.todos.map(todo => (
+            <Todo
+              onToggle={() => this.toggleTodo(todo.id)}
+              onDelete={() => this.removeTodo(todo.id)}
+              todo={todo}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+render(<CApp />, document.getElementById("root"));
